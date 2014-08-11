5ef233533d423e0d1c2f3f1285a7cce98c8b54c8
Aaron Johnson <aaronjohnsonis@gmail.com>
2014-08-10 22:36:27 -0500

    alt home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'home_alt' => 'pages#home_alt'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
