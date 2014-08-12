9644e632eebb98ada46351d36c3c3c92e3c642fd
Aaron Johnson <aaronjohnsonis@gmail.com>
2014-08-12 11:09:24 -0500

    style boxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get 'home_alt' => 'pages#home_alt'
+  root to: 'pages#home_alt'
+  get 'home' => 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
